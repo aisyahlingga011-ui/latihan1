01efdc97de5d542c9d28bdc2527ae101d059dcd0
aisyahlingga011-ui <aisyahlingga011@gmail.com>
2026-09-22 16:24:37 +0700

    Add files via upload

---
+++ a86.py
@@ -0,0 +1,12 @@
+if grade >= 65:
+    print("passed the exam")
+else:
+    print("below the passing grade")
+
+    if grade >= 65: print("passed the exam")
+if grade < 65: print("below the passing grade")
+
+print("passed the exam") if grade >= 65 else print("below the passing grade")
+
+message = "passed the exam" if grade >= 65 else "below the passing grade"
+print(message)
---
+++ fungsiinput.py
@@ -0,0 +1,2 @@
+str_input = input('Enter your grade: ')
+print("Inputan user:", str_input, type(str_input))
